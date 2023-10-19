156d3694733e6c8f84afdca8390d18e16d857835
srira <srira@Divya_ACER>
2023-10-19 14:26:58 +0530

    first commit in git

---
+++ webEntityEx1/src/main/java/com/example/demo/WebEntityEx1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEntityEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEntityEx1Application.class, args);
+	}
+
+}
